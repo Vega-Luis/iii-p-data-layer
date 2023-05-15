c32b14b066964d313ab252b7ac898f1305bc7329
Justin Acuña <justingilberto2000@gmail.com>
2023-05-14 21:06:42 -0600

    feat: add insertion CTM in database

--- operation-processing.sql
+++ operation-processing.sql
@@ -85,19 +85,43 @@ BEGIN
 
 	INSERT INTO @InputCTMaster(
 		Code
-		, [CTMasterType]
+		, [AccountType]
 		, [CreditLimit]
 		, [Value]
 	)
 	SELECT
 		CTM.Item.VALUE('@Codigo', 'INT')
-		, CTM.Item.VALUE('@CTMasterType', 'VARCHAR(16)')
-		, CTM.Item.VALUE('@CreditLimit', 'MONEY')
-		, CTM.Item.VALUE('@Value', 'VARCHAR(16)')
+		, CTM.Item.VALUE('@TipoCTM', 'VARCHAR(16)')
+		, CTM.Item.VALUE('@LimiteCredito', 'MONEY')
+		, CTM.Item.VALUE('@TH', 'VARCHAR(16)')
 	FROM @xmlData.NODES(
 		'(root/fechaOperacion[@Fecha=sql:variable("@ActualDate")]/NTCM/NTCM)'
 	)
 	AS CTM(Item)
 
 	SET @ActualRecord = @ActualRecord + 1;
+
+	-- Insertion in Database
+	INSERT dbo.MasterAccount(
+		Id
+		, IdCardHolder
+		, IdAccountType
+		, CreditLimit
+	)
+	SELECT 
+		MA.Id
+		, CH.Id
+		, TY.Id
+		, CTM.CreditLimit
+	FROM dbo.MasterAccount MA
+	INNER JOIN 
+	@InputCTMaster CTM
+		ON MA.Code = CTM.Code
+	INNER JOIN
+	dbo.CardHolder CH
+		ON CH.Value = CTM.Value
+	INNER JOIN
+	dbo.AccountType TY
+		ON CTM.AccountType = TY.Name
+
 END
