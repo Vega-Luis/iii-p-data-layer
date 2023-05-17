1af20fd04a30193bb3ddff035b8732c17f7bf26f
Vega-Luis <lanvega76r@gmail.com>
2023-05-16 18:49:54 -0600

    feat: inserting new card holders

--- operation-processing.sql
+++ operation-processing.sql
@@ -6,6 +6,29 @@ DECLARE
 	, @ActualDate DATE
 	;
 
+-- Iteration variables
+DECLARE
+	@ActualIndex INT
+	, @LastIndex INT
+	;
+
+-- Card holder insertion variables
+DECLARE
+	@CardHolderName VARCHAR(64)
+	, @DocumentTypeName VARCHAR(16)
+	, @IdentificationValue VARCHAR(16)
+	, @Username VARCHAR(16)
+	, @Password VARCHAR(16)
+	, @CardHolderUserType INT
+	, @ActualCardHolderId INT
+	, @DocumentTypeId INT
+	;
+
+-- Getting card holder user type
+SELECT @CardHolderUserType = UT.Id
+FROM dbo.UserType UT
+WHERE UT.[Name] = 'Targeta Habiente'
+
 -- Temp table to load operation tables from xml
 DECLARE @Dates TABLE (
 	Sec INT IDENTITY(1,1)
@@ -16,9 +39,9 @@ DECLARE @Dates TABLE (
 DECLARE @InputCardHolder TABLE (
 		Sec INT IDENTITY(1,1)
 		, [Name]	VARCHAR(64)
-		, [DocumentType] VARCHAR(16)
+		, [DocumentTypeName] VARCHAR(16)
 		, [IdentificationValue] VARCHAR(16)
-		, [UserName] VARCHAR(16)
+		, [Username] VARCHAR(16)
 		, [Password] VARCHAR(16)
 	)
 	;
@@ -55,26 +78,87 @@ BEGIN
 	FROM @Dates D
 	WHERE D.Sec = @ActualRecord
 	
+	-- Begins card holder operations
 	-- Clean table for new operation date
 	DELETE @InputCardHolder
 	
+	-- Preprocess new card holders
 	INSERT INTO @InputCardHolder (
 		[Name]
-		, [DocumentType]
+		, [DocumentTypeName]
 		, [IdentificationValue]
-		, [UserName]
+		, [Username]
 		, [Password]
 	)
 	SELECT
 		T.Item.value('@Nombre', 'VARCHAR(64)')
 		, T.Item.value('@Tipo_Doc_Identidad', 'VARCHAR(16)')
 		, T.Item.value('@Valor_Doc_Identidad', 'VARCHAR(16)')
-		, T.Item.value('@NombreUsusario', 'VARCHAR(16)')
+		, T.Item.value('@NombreUsuario', 'VARCHAR(16)')
 		, T.Item.value('@Password', 'VARCHAR(16)')
 	FROM @xmlData.nodes(
 		'(root/fechaOperacion[@Fecha=sql:variable("@ActualDate")]/TH/TH)'
 		)
 	AS T(Item)
+
+	-- Set Iteration floor
+	SELECT @ActualIndex = MIN(ICH.Sec)
+	FROM @InputCardHolder ICH
+
+	-- Set iteration ceil
+	SELECT @LastIndex = MAX(ICH.Sec)
+	FROM @InputCardHolder ICH
+
+	-- Begins insert iteration
+	WHILE (@ActualIndex <= @LastIndex)
+	BEGIN
+		SELECT
+			@CardHolderName = ICH.[Name]
+			, @DocumentTypeName = ICH.DocumentTypeName
+			, @IdentificationValue = ICH.IdentificationValue
+			, @Username = ICH.Username
+			, @Password = ICH.[Password]
+		FROM @InputCardHolder ICH
+		WHERE ICH.Sec = @ActualIndex
+
+		-- Insert user
+		INSERT INTO dbo.[User] (
+			[IdUserType]
+			, [Username]
+			, [Password]
+		)
+		VALUES (
+			@CardHolderUserType
+			, @Username
+			, @Password
+		)
+
+		-- Catch User id
+		SET @ActualCardHolderId = SCOPE_IDENTITY();
+		
+		-- obtain document type id
+		SELECT @DocumentTypeId = DT.Id
+		FROM dbo.DocumentType DT
+		WHERE DT.Name = @DocumentTypeName
+
+		-- Insert into card holder table
+		INSERT INTO dbo.CardHolder (
+			[Id]
+			, [IdDocumentType]
+			, [Name]
+			, [Value]
+		)
+		VALUES (
+			@ActualCardHolderId
+			, @DocumentTypeId
+			, @CardHolderName
+			, @IdentificationValue
+		)
+
+		
+		SET @ActualIndex = @ActualIndex + 1
+	END
+	-- Ends car holder operations
 
 	SET @ActualRecord = @ActualRecord + 1;
 END
